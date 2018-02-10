2a27db395f7bd6eca9cb4d43e4b03f1c5b80e864
Alex Bachuk <alex@bachuk.com>
2018-02-10 16:35:05 -0500

    single blog post

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,73 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
 
- // You can delete this file if you're not using it
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+
+  return new Promise((resolve, reject) => {
+    const blogPost = path.resolve('./src/templates/blog-post.js')
+    resolve(
+      graphql(
+        `
+          {
+            allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }, limit: 100) {
+              edges {
+                node {
+                  fields {
+                    slug
+                  }
+                  frontmatter {
+                    title
+                    slug
+                    url
+                  }
+                }
+              }
+            }
+          }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+
+        // Create blog posts pages.
+        const posts = result.data.allMarkdownRemark.edges;
+
+        posts.forEach((post, index) => {
+          const previous = index === posts.length - 1 ? false : posts[index + 1].node;
+          const next = index === 0 ? false : posts[index - 1].node;
+          console.log(post);
+          // post.node.fields.slug
+          createPage({
+            path: post.node.frontmatter.slug,
+            component: blogPost,
+            context: {
+              slug: post.node.frontmatter.slug,
+              url: post.node.frontmatter.url,
+              previous,
+              next,
+            },
+          })
+        })
+      })
+    )
+  })
+}
+
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+  const { createNodeField } = boundActionCreators
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
